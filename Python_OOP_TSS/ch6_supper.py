9b09bb82ca377252162bd69253f1605d49dce336
Gazi Ashiq <gaziashiq.dev@gmail.com>
2023-10-25 02:33:46 +0600

    class method overriding → super()

---
+++ Python_OOP_TSS/ch6_supper.py
@@ -0,0 +1,25 @@
+class Vehicle:
+    """
+    Base class for all vehicles
+    """
+
+    def __init__(self, name, manufacturer, color):
+        print('Creating a vehicle')
+        self.name = name
+        self.manufacturer = manufacturer
+        self.color = color
+
+
+class Car(Vehicle):
+    """CLass: Car, Inherit: Vehicle class"""
+
+    def __init__(self, name, manufacturer, color, year):
+        print('Creating a car')
+        super().__init__(name, manufacturer, color)
+        self.year = year
+        self.wheels = 4
+
+
+if __name__ == '__main__':
+    c = Car('Mustang', 'Ford', 'Red', 2017)
+    print(c.name, c.year, c.wheels)
