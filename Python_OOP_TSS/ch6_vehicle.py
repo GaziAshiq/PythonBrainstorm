68a7e650e160e549321fb5109e0f9da92faf35ab
Gazi Ashiq <gaziashiq.dev@gmail.com>
2023-10-25 01:56:11 +0600

    inheritance

--- Python_OOP_TSS/ch6_vehicle.py
+++ Python_OOP_TSS/ch6_vehicle.py
@@ -18,18 +18,30 @@ class Vehicle:
         print(f'{self.name} is stopping!')
 
 
+class Car(Vehicle):
+    """CLass: Car, Inherit: Vehicle class"""
+
+    def change_gear(self, gear_name):
+        print(f'{self.name} changing gear to {gear_name}')
+
+
 if __name__ == '__main__':
-    v1 = Vehicle('MT-15', 'Yamaha', 'Glossy Black')
-    v2 = Vehicle('GSX-S150', 'Suzuki', 'Yellow')
-    v3 = Vehicle('Hunter 350', 'Royal Enfield', 'Blue-White')
+    # v1 = Vehicle('MT-15', 'Yamaha', 'Glossy Black')
+    # v2 = Vehicle('GSX-S150', 'Suzuki', 'Yellow')
+    # v3 = Vehicle('Hunter 350', 'Royal Enfield', 'Blue-White')
+    #
+    # v1.drive()
+    # v2.drive()
+    # v3.drive()
+    #
+    # v1.turn('right')
+    # v2.turn('left')
+    #
+    # v1.brake()
+    # v2.brake()
+    # v3.brake()
 
-    v1.drive()
-    v2.drive()
-    v3.drive()
-
-    v1.turn('right')
-    v2.turn('left')
-
-    v1.brake()
-    v2.brake()
-    v3.brake()
+    c1 = Car('Mustang', 'Ford', 'Black')
+    c1.drive()
+    c1.brake()
+    c1.change_gear('P')
